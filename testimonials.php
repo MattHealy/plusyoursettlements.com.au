6bc8bf6cc52f149b34ab042da94d9f324a2783e3
Matt Healy <healmatt@gmail.com>
2017-11-27 21:10:23 +0800

    Update testimonials

--- testimonials.php
+++ testimonials.php
@@ -30,6 +30,10 @@
 "Thank you Sam, you are a miracle worker." - <span class="bold">Joyce</span>
 					</p>
 
+					<p>
+"I couldn't be happier with the professional and down to earth service yourself and real estate plus provided. Thoroughly recommended and if the need arose again I wouldn't go anywhere else thanks again" - <span class="bold">Stuart Lewington</span>
+					</p>
+
 				</div>
 	
 <?php require_once('sidebar.php') ?>
