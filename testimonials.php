6f576b166fff0864bd81c877c32ae75c5a8a2d44
Matt Healy <healmatt@gmail.com>
2017-11-28 20:46:54 +0800

    Update testimonials

--- testimonials.php
+++ testimonials.php
@@ -34,6 +34,59 @@
 "I couldn't be happier with the professional and down to earth service yourself and real estate plus provided. Thoroughly recommended and if the need arose again I wouldn't go anywhere else thanks again" - <span class="bold">Stuart Lewington</span>
 					</p>
 
+					<p>
+"Can I say Sam you made this transition very easy for both Annika &amp; I.
+We really appreciate your effort in organising and obtain the
+necessary paperwork to sign, very professional I must say.
+It's a breath of fresh air when you can rely on a company
+like yours that takes all the hassles out of buying or
+transferring a property." - <span class="bold">Steve &amp; Annika</span>
+					</p>
+
+					<p>
+"Michelle, thank you very much for all your help with the settlement of my property in Mount Helena." - <span class="bold">Peter</span>
+					</p>
+
+					<p>
+"A big thank you for the speedy and efficent way you handled the sale of our property." - <span class="bold">Jan</span>
+					</p>
+
+					<p>
+"Your service was excellent, communication was great, answered all of my questions in a timely manner.
+You helped me understand a few things that I wasn't sure about.
+As a first homebuyer it was a bit daunting but with your help everything seemed to go really smoothly." - <span class="bold">Dan</span>
+					</p>
+
+					<p>
+"Michelle, I just wanted to say a big thank you for all
+you did during the selling of our house in Parkerville,
+and purchase in Wundowie.
+Your kind proficant manner was a joy to see, and to
+do it all without any problems in such a short space
+of time was wonderful." - <span class="bold">Sheila &amp; Keith</span>
+					</p>
+
+					<p>
+"Thank you very much for everything. Sam and his
+team were wonderful and professional. It made a
+very difficult time much easier and the settlement
+went very smoothly." - <span class="bold">Sarah</span>
+					</p>
+
+					<p>
+"Sam of Plus Your Settlements
+was the rock that we needed
+to assist us with the sale of our
+house." - <span class="bold">Malcolm</span>
+					</p>
+
+					<p>
+"Thank you very much for all
+your help, Michelle. It was
+very much appreciated." - <span class="bold">Jenny &amp; Calvin</span>
+					</p>
+
+
 				</div>
 	
 <?php require_once('sidebar.php') ?>
