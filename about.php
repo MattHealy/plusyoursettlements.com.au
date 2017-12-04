f655bb1cffcf58cf3a0ceeacd7c9077b86d9aa11
Matt Healy <healmatt@gmail.com>
2017-12-04 20:06:38 +0800

    Update about us page with images

--- about.php
+++ about.php
@@ -18,8 +18,6 @@
 				
 				<div class="left">
 											
-					<p class="intro-text">About Us</p>
-						
 <p>
 The Plus Your Settlements team helps in all aspects of settlements:
 </p>
@@ -37,7 +35,48 @@ The Plus Your Settlements team helps in all aspects of settlements:
 
 <p>We are local for the Swan, Kalamunda, Mundaring and Toodyay shires so come and visit us and discuss your needs. Plus Your Settlements understands the importance of getting things right, so please chat with us today on 08 9274 2833.</p>
 
-										
+<h2>Our objective</h2>
+
+<p>To provide the right advice and information for our customers to make an informed decision about their property needs.</p>
+
+<p>We stand by this objective as we believe this is a strong moral and ethical standing to operate a successful business from. We believe that if our customers have all the knowledge available to make a decision about their property needs this builds trust and rapport which we can grow our business around.</p>
+
+<h2>Who are Plus Your Settlements</h2>
+
+  <img src="images/office.jpg" width="500" height="333" alt="Plus Your Settlements Team photo" />
+
+  <p>Plus Your Settlements is a residential and commercial property settlement agent located at 11 The Crescent, Midland, Western Australia. Plus Your Settlements commenced operation in 2010 servicing customers all over the great state of Western Australia. Currently the team at Plus Your Settlements is made up of Sam Winter and Michelle Schorer.</p>
+
+<div style="float: left; width: 50%">
+
+  <h3>Sam Winter - Licensee</h3>
+
+  <p style="width: 100%">Sam joined Plus Your Settlements in 2015. Sam has a wide range of skills drawing on his time in the banking, legal, property finance and settlement industry. This range of experience Sam has is to your benefit, Sam’s knowledge of how other parties involved in the transaction can put your mind at ease when it all comes to be a little bit too confusing. Sam obtained his Settlement agents license in 2011 and has been actively employed in the industry since 2003. Sam lives locally in the Greenmount area and has a very sound understanding of what is required to manage your expectations and conduct Settlements rural or urban.</p>
+
+</div>
+
+<div style="float: right; width: 50%">
+  <img src="images/samwinter.jpg" width="200" alt="Sam Winter photo" style="margin-left: 30px;" />
+</div>
+
+<div style="clear: both;">&nbsp;</div>
+
+<div style="float: left; width: 40%">
+
+  <img src="images/michelleschorer.jpg" width="200" alt="Michelle Schorer photo" />
+
+</div>
+
+<div style="float: right; width: 60%">
+
+  <h3>Michelle Schorer – Conveyancer</h3>
+
+  <p style="width: 100%">Michelle joined Plus Your Settlements in 2015. Michelle comes from a background in property management and real estate. Michelle has been in the Settlement industry since 2013. Michelle lives locally in Chidlow and is a highly competent Conveyancer with a smile that lights up any room, you will love dealing with Michelle.</p>
+
+</div>
+
+<div style="clear: both;">&nbsp;</div>
+
 				</div>
 				
 <?php require_once('sidebar.php') ?>
