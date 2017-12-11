7f754141e98d6bd17eb78d5ad5e3d9975d26ad08
Matt Healy <healmatt@gmail.com>
2017-12-11 20:39:41 +0800

    Add links page - still needs better formatting

---
+++ links.php
@@ -0,0 +1,43 @@
+<?php $pagetitle = 'Links' ?>
+<?php require_once('head.php') ?>
+
+		<div id="primaryContent">
+			<div class="content">
+				<div class="pageTop">
+					<div class="pageTitle">
+						<h1>Links</h1>
+					</div>
+					<div class="breadCrumbs">
+						You are here: <a href="/" title="Home">Home</a>
+
+						&raquo;&nbsp;<span class="active">Links</span>
+							
+					</div>					
+					<div class="clearer"></div>
+				</div>
+				
+				<div class="left">
+											
+					<p class="intro-text">Handy links</p>
+						
+					<ul>
+					   <li>Our Facebook page - Like and be our Facebook friend - <a href="https://www.facebook.com/plusyoursettlements/" target="_blank">https://www.facebook.com/plusyoursettlements/</a></li>
+					   <li>Australian Taxation Office - Foreign residents capital gains withholding information, important for property sales over $750,000.00 - <a href="https://www.ato.gov.au/General/Capital-gains-tax/In-detail/Calculating-a-capital-gain-or-loss/Foreign-resident-capital-gains-withholding-payments---common-questions/" target="_blank">https://www.ato.gov.au/General/Capital-gains-tax/In-detail/Calculating-a-capital-gain-or-loss/Foreign-resident-capital-gains-withholding-payments---common-questions/</a></li>
+					   <li>Department of Mines, Industry Regulation and Safety - Home Buyers assistance account, for first home buyers purchasing under $400,000.00 an additional $2,000.00 grant may be available - <a href="https://www.commerce.wa.gov.au/consumer-protection/home-buyers-assistance-account" target="_blank">https://www.commerce.wa.gov.au/consumer-protection/home-buyers-assistance-account</a></li>
+					   <li>Department of Mines, Industry Regulation and Safety - Rules for pools and spa, important for owners of pools and spas - <a href="https://www.commerce.wa.gov.au/publications/rules-pools-and-spas" target="_blank">https://www.commerce.wa.gov.au/publications/rules-pools-and-spas</a></li>
+					   <li>Department of Mines, Industry Regulation and Safety - Statutory legislation for electrical safety compliance for residual current devices (RCD and hardwired smoke alarms) - <a href="https://www.commerce.wa.gov.au/consumer-protection/smoke-alarms-and-rcds" target="_blank">https://www.commerce.wa.gov.au/consumer-protection/smoke-alarms-and-rcds</a></li>
+					   <li>Department of Transport - Jetty licenses - <a href="https://www.transport.wa.gov.au/imarine/jetty-licences.asp" target="_blank">https://www.transport.wa.gov.au/imarine/jetty-licences.asp</a></li>
+					   <li>Foreign Investment Review Board - Information on being a foreign investor - <a href="http://firb.gov.au/" target="_blank">http://firb.gov.au/</a></li>
+					   <li>Property Exchange Australia - Information about e-conveyancing and settling online - <a href="https://www.pexa.com.au/buyers-sellers" target="_blank">https://www.pexa.com.au/buyers-sellers</a></li>
+					   <li>Title insurance products - Insurance that covers you if there are unimproved structures or incorrect boundaries in place - <a href="http://www.stewartau.com/" target="_blank">http://www.stewartau.com/</a> <a href="http://www.firsttitle.com.au/home" target="_blank">http://www.firsttitle.com.au/home</a></li>
+					</ul>
+
+				</div>
+
+<?php require_once('sidebar.php') ?>
+
+			</div>
+		</div>
+		</div>
+
+<?php require_once('tail.php') ?>
