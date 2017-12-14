1a3c2252adf48a9d60877966725a974ca3a3daf4
Matt Healy <healmatt@gmail.com>
2017-12-14 19:48:46 +0800

    Restyled links page

--- links.php
+++ links.php
@@ -21,15 +21,15 @@
 					<p class="intro-text">Handy links</p>
 						
 					<ul>
-					   <li>Our Facebook page - Like and be our Facebook friend - <a href="https://www.facebook.com/plusyoursettlements/" target="_blank">https://www.facebook.com/plusyoursettlements/</a></li>
-					   <li>Australian Taxation Office - Foreign residents capital gains withholding information, important for property sales over $750,000.00 - <a href="https://www.ato.gov.au/General/Capital-gains-tax/In-detail/Calculating-a-capital-gain-or-loss/Foreign-resident-capital-gains-withholding-payments---common-questions/" target="_blank">https://www.ato.gov.au/General/Capital-gains-tax/In-detail/Calculating-a-capital-gain-or-loss/Foreign-resident-capital-gains-withholding-payments---common-questions/</a></li>
-					   <li>Department of Mines, Industry Regulation and Safety - Home Buyers assistance account, for first home buyers purchasing under $400,000.00 an additional $2,000.00 grant may be available - <a href="https://www.commerce.wa.gov.au/consumer-protection/home-buyers-assistance-account" target="_blank">https://www.commerce.wa.gov.au/consumer-protection/home-buyers-assistance-account</a></li>
-					   <li>Department of Mines, Industry Regulation and Safety - Rules for pools and spa, important for owners of pools and spas - <a href="https://www.commerce.wa.gov.au/publications/rules-pools-and-spas" target="_blank">https://www.commerce.wa.gov.au/publications/rules-pools-and-spas</a></li>
-					   <li>Department of Mines, Industry Regulation and Safety - Statutory legislation for electrical safety compliance for residual current devices (RCD and hardwired smoke alarms) - <a href="https://www.commerce.wa.gov.au/consumer-protection/smoke-alarms-and-rcds" target="_blank">https://www.commerce.wa.gov.au/consumer-protection/smoke-alarms-and-rcds</a></li>
-					   <li>Department of Transport - Jetty licenses - <a href="https://www.transport.wa.gov.au/imarine/jetty-licences.asp" target="_blank">https://www.transport.wa.gov.au/imarine/jetty-licences.asp</a></li>
-					   <li>Foreign Investment Review Board - Information on being a foreign investor - <a href="http://firb.gov.au/" target="_blank">http://firb.gov.au/</a></li>
-					   <li>Property Exchange Australia - Information about e-conveyancing and settling online - <a href="https://www.pexa.com.au/buyers-sellers" target="_blank">https://www.pexa.com.au/buyers-sellers</a></li>
-					   <li>Title insurance products - Insurance that covers you if there are unimproved structures or incorrect boundaries in place - <a href="http://www.stewartau.com/" target="_blank">http://www.stewartau.com/</a> <a href="http://www.firsttitle.com.au/home" target="_blank">http://www.firsttitle.com.au/home</a></li>
+					   <li><a href="https://www.facebook.com/plusyoursettlements/" target="_blank">Our Facebook page</a> - Like and be our Facebook friend</li>
+					   <li><a href="https://www.ato.gov.au/General/Capital-gains-tax/In-detail/Calculating-a-capital-gain-or-loss/Foreign-resident-capital-gains-withholding-payments---common-questions/" target="_blank">Australian Taxation Office</a> - Foreign residents capital gains withholding information, important for property sales over $750,000.00</li>
+					   <li><a href="https://www.commerce.wa.gov.au/consumer-protection/home-buyers-assistance-account" target="_blank">Department of Mines, Industry Regulation and Safety</a> - Home Buyers assistance account, for first home buyers purchasing under $400,000.00 an additional $2,000.00 grant may be available</li>
+					   <li><a href="https://www.commerce.wa.gov.au/publications/rules-pools-and-spas" target="_blank">Department of Mines, Industry Regulation and Safety</a> - Rules for pools and spa, important for owners of pools and spas</li>
+					   <li><a href="https://www.commerce.wa.gov.au/consumer-protection/smoke-alarms-and-rcds" target="_blank">Department of Mines, Industry Regulation and Safety</a> - Statutory legislation for electrical safety compliance for residual current devices (RCD and hardwired smoke alarms)</li>
+					   <li><a href="https://www.transport.wa.gov.au/imarine/jetty-licences.asp" target="_blank">Department of Transport</a> - Jetty licenses</li>
+					   <li><a href="http://firb.gov.au/" target="_blank">Foreign Investment Review Board</a> - Information on being a foreign investor</li>
+					   <li><a href="https://www.pexa.com.au/buyers-sellers" target="_blank">Property Exchange Australia</a> - Information about e-conveyancing and settling online</li>
+					   <li><a href="http://www.stewartau.com/" target="_blank">Title insurance products</a> - Insurance that covers you if there are unimproved structures or incorrect boundaries in place</li>
 					</ul>
 
 				</div>
